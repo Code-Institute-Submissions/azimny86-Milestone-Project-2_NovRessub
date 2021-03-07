baff62ccf2495d57b44f4787fb44d2c62af021ab
azimny86 <azimny86@gmail.com>
2021-03-07 15:17:51 +0000

    Navbar and slide show added

---
+++ assets/scripts/slideshow.js
@@ -0,0 +1,14 @@
+//  Slideshow
+
+var slides= ['assets/images/slide1.jpg','assets/images/slide2.jpg','assets/images/slide3.jpg','assets/images/slide4.jpg','assets/images/slide5.jpg'];
+
+var i = 0;
+
+function slideshow() {
+    document.getElementById("slide").src = slides[i];
+    i++;
+    if(i>4) {
+        i=0
+    }
+}
+setInterval(slideshow, 1000)
